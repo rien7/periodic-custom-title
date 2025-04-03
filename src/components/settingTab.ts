8127518a265495c0840ae598543e1305bf3b1c01
RieN 7z <zrien7@gmail.com>
2025-04-03 19:18:36 +0800

    feat: add suggestion item

---
+++ src/components/settingTab.ts
@@ -0,0 +1,268 @@
+import { App, PluginSettingTab, Setting } from "obsidian";
+import PeriodicCustomTitle, { PeriodicCustomTitleSettings } from "src/main";
+import { PeriodicReturn, PeriodicType } from "src/utils/get-periodic-config";
+import { PeriodicSetting, renderTitle } from "src/utils/title-template";
+
+export const DEFAULT_PERIODIC_SETTING: PeriodicSetting = {
+  dateFormat: "",
+  dateFormatNotInThisYear: "",
+  relativeSettings: [],
+  relativePosition: "before",
+  separator: " ",
+};
+
+export class SampleSettingTab extends PluginSettingTab {
+  plugin: PeriodicCustomTitle;
+
+  constructor(app: App, plugin: PeriodicCustomTitle) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  async hide(): Promise<void> {
+    super.hide();
+    await this.plugin.saveSettings();
+    this.plugin.handleFileOpen();
+  }
+
+  display(): void {
+    const { containerEl } = this;
+
+    const locales = window.moment.locales().reduce(
+      (acc, locale) => {
+        acc[locale] = locale;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+
+    containerEl.empty();
+
+    new Setting(containerEl).setName("Language").addDropdown((cp) =>
+      cp
+        .addOptions(locales)
+        .setValue(this.plugin.settings.language)
+        .onChange((value) => {
+          this.plugin.settings.language = value;
+          window.moment.updateLocale(value, {
+            week: { dow: this.plugin.settings.firstDayOfWeek },
+          });
+        }),
+    );
+
+    new Setting(containerEl).setName("First day of week").addDropdown((cp) =>
+      cp
+        .addOption("7", "Sunday")
+        .addOption("1", "Monday")
+        .setValue(this.plugin.settings.firstDayOfWeek.toString())
+        .onChange((_value) => {
+          const value = parseInt(_value) as 1 | 7;
+          this.plugin.settings.firstDayOfWeek = value;
+          window.moment.updateLocale(this.plugin.settings.language, {
+            week: { dow: value },
+          });
+        }),
+    );
+
+    // [INFO] Periodic Notes Settings
+    (
+      [
+        ["Daily Notes", "daily"],
+        ["Weekly Notes", "weekly"],
+        ["Monthly Notes", "monthly"],
+        ["Yearly Notes", "yearly"],
+      ] as const
+    ).forEach(([name, key]) => {
+      const exampleDom = document.createElement("div");
+      const exampleFragment = document.createDocumentFragment();
+      exampleFragment.appendChild(exampleDom);
+
+      const render = () => renderExample(key, this.plugin.settings, exampleDom);
+
+      render();
+
+      // [INFO] Toggle
+      const titleSetting = new Setting(containerEl)
+        .setName(name)
+        .setHeading()
+        .addToggle((cp) =>
+          cp.setValue(this.plugin.settings[key].enabled).onChange((value) => {
+            this.plugin.settings[key].enabled = value;
+            if (!value) {
+              this.plugin.settings[key].setting = DEFAULT_PERIODIC_SETTING;
+            }
+            render();
+          }),
+        );
+      titleSetting.nameEl.style.fontSize = "1.5em";
+      titleSetting.nameEl.style.fontWeight = "bold";
+      titleSetting.nameEl.style.padding = "0.5em 0";
+      const exampleSetting = new Setting(containerEl)
+        .setName("Example")
+        .setDesc(exampleFragment);
+      const exampleSettingDescEl = exampleSetting.descEl;
+      exampleSetting.infoEl.childNodes[1].remove();
+      exampleSetting.controlEl.appendChild(exampleSettingDescEl);
+      exampleSetting.controlEl.style.textAlign = "start";
+
+      // [INFO] Date Format
+      const syntaxNode = document.createElement("a");
+      syntaxNode.href = "https://momentjs.com/docs/#/displaying/format/";
+      syntaxNode.textContent = "Syntax Reference";
+      const syntaxFragment = new DocumentFragment();
+      syntaxFragment.appendChild(syntaxNode);
+      new Setting(containerEl)
+        .setName("Date Format")
+        .setDesc(syntaxFragment)
+        .addMomentFormat((cp) =>
+          cp
+            .setValue(this.plugin.settings[key].setting.dateFormat)
+            .onChange((value) => {
+              this.plugin.settings[key].setting.dateFormat = value;
+              render();
+            }),
+        );
+
+      // [INFO] Date Format Not In This Year
+      new Setting(containerEl)
+        .setName("Date Format Not In This Year")
+        .setDesc(syntaxFragment)
+        .addMomentFormat((cp) =>
+          cp
+            .setValue(this.plugin.settings[key].setting.dateFormatNotInThisYear)
+            .onChange((value) => {
+              this.plugin.settings[key].setting.dateFormatNotInThisYear = value;
+              render();
+            }),
+        );
+
+      // [INFO] Separator
+      new Setting(containerEl).setName("Separator").addText((cp) =>
+        cp
+          .setValue(this.plugin.settings[key].setting.separator)
+          .onChange((value) => {
+            this.plugin.settings[key].setting.separator = value;
+            render();
+          }),
+      );
+
+      // [INFO] Relative Position
+      new Setting(containerEl).setName("Relative Position").addDropdown((cp) =>
+        cp
+          .addOption("before", "Before Date")
+          .addOption("after", "After Date")
+          .setValue(this.plugin.settings[key].setting.relativePosition)
+          .onChange((value) => {
+            this.plugin.settings[key].setting.relativePosition = value as
+              | "before"
+              | "after";
+            render();
+          }),
+      );
+
+      // [INFO] Relative Settings
+      new Setting(containerEl)
+        .setName("Relative Settings")
+        .setDesc("Format: diff | text | color(optional)")
+        .addTextArea((cp) =>
+          cp
+            .setPlaceholder(`0 | xxx\n-1 | xxx | red`)
+            .setValue(
+              this.plugin.settings[key].setting.relativeSettings
+                .map((setting) =>
+                  [setting.diff, setting.text, setting.color]
+                    .filter((s) => s !== undefined)
+                    .join(" | "),
+                )
+                .join("\n"),
+            )
+            .onChange((value) => {
+              const items = value.split("\n");
+              const relativeSettings = [];
+              for (const item of items) {
+                const [diff, text, color] = item
+                  .split("|")
+                  .map((i) => i.trim());
+                if (diff && text) {
+                  relativeSettings.push({
+                    diff: parseInt(diff),
+                    text,
+                    color,
+                  });
+                }
+              }
+              this.plugin.settings[key].setting.relativeSettings =
+                relativeSettings;
+              render();
+            }),
+        );
+    });
+  }
+}
+
+function renderExample(
+  type: PeriodicType,
+  settings: PeriodicCustomTitleSettings,
+  target: HTMLDivElement,
+) {
+  target.innerHTML = "";
+  if (!settings[type].enabled) {
+    return;
+  }
+  const typeUnit: Map<PeriodicType, moment.unitOfTime.DurationConstructor> =
+    new Map([
+      ["daily", "d"],
+      ["weekly", "w"],
+      ["monthly", "M"],
+      ["yearly", "y"],
+    ]);
+  const regularText = document.createElement("div");
+  regularText.textContent = "Regular Text";
+  regularText.style.fontWeight = "600";
+  regularText.style.padding = "4px 0";
+  target.appendChild(regularText);
+
+  const lastYear = document.createElement("div");
+  lastYear.style.paddingLeft = "12px";
+  const lastYearDate = window.moment().add(-1, "y").month(0).date(1);
+  renderTitle(
+    { type, date: lastYearDate },
+    settings[type].setting,
+    lastYear,
+    settings.language,
+  );
+  target.appendChild(lastYear);
+
+  const thisYear = document.createElement("div");
+  thisYear.style.paddingLeft = "12px";
+  const thisYearDate = window.moment().month(0).date(1);
+  renderTitle(
+    { type, date: thisYearDate },
+    settings[type].setting,
+    thisYear,
+    settings.language,
+  );
+  target.appendChild(thisYear);
+
+  const datas: PeriodicReturn[] = settings[type].setting.relativeSettings.map(
+    (setting) => {
+      const date = window.moment().add(setting.diff, typeUnit.get(type));
+      return { type, date };
+    },
+  );
+
+  if (datas.length > 0) {
+    const relativeText = document.createElement("div");
+    relativeText.textContent = "Relative Text";
+    relativeText.style.fontWeight = "600";
+    relativeText.style.padding = "4px 0";
+    target.appendChild(relativeText);
+  }
+
+  for (const data of datas) {
+    const dom = document.createElement("div");
+    dom.style.paddingLeft = "12px";
+    renderTitle(data, settings[type].setting, dom, settings.language);
+    target.appendChild(dom);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { App, FileView, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { App, FileView, Plugin, TFile } from "obsidian";
 import { handleFileOpen, removeRoot } from "src/components/alias-title";
-import { renderTitle, PeriodicSetting } from "src/utils/title-template";
-import { PeriodicReturn, PeriodicType } from "src/utils/get-periodic-config";
+import { PeriodicSetting } from "src/utils/title-template";
+import { getPathByDate, PeriodicType } from "src/utils/get-periodic-config";
+import {
+  DEFAULT_PERIODIC_SETTING,
+  SampleSettingTab,
+} from "./components/settingTab";
 
 export interface PeriodicConfig {
   enabled: boolean;
   setting: PeriodicSetting;
 }
 
-interface PeriodicCustomTitleSettings {
+export interface PeriodicCustomTitleSettings {
   language: string;
   firstDayOfWeek: 1 | 7;
   daily: PeriodicConfig;
@@ -16,14 +20,6 @@ interface PeriodicCustomTitleSettings {
   monthly: PeriodicConfig;
   yearly: PeriodicConfig;
 }
-
-const DEFAULT_PERIODIC_SETTING: PeriodicSetting = {
-  dateFormat: "",
-  dateFormatNotInThisYear: "",
-  relativeSettings: [],
-  relativePosition: "before",
-  separator: " ",
-};
 
 const DEFAULT_SETTINGS: PeriodicCustomTitleSettings = {
   language: "en",
@@ -51,10 +47,29 @@ export default class PeriodicCustomTitle extends Plugin {
     this.app.workspace.on("file-open", this.handleFileOpen);
 
     this.addSettingTab(new SampleSettingTab(this.app, this));
+
+    getAllRelativeSetting(this.app, this.settings).then((files) => {
+      let cache = this.app.metadataCache as any;
+      let _getLinkSuggestions = cache.getLinkSuggestions;
+      cache._getLinkSuggestions = _getLinkSuggestions;
+      let _wrapperGetLinkSuggestions = function () {
+        let result = _getLinkSuggestions.call(this);
+        return [
+          ...files.map((file) => ({
+            path: file.text,
+            file: file.file,
+          })),
+        ].concat(result);
+      };
+      cache.getLinkSuggestions = _wrapperGetLinkSuggestions;
+    });
   }
 
   onunload() {
     this.app.workspace.off("file-open", this.handleFileOpen);
+    let cache = this.app.metadataCache as any;
+    cache.getLinkSuggestions = cache._getLinkSuggestions;
+    cache._getLinkSuggestions = null;
     document
       .querySelectorAll('.workspace-leaf-content[data-type="markdown"]')
       .forEach((el) => {
@@ -71,258 +86,38 @@ export default class PeriodicCustomTitle extends Plugin {
   }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-  plugin: PeriodicCustomTitle;
-
-  constructor(app: App, plugin: PeriodicCustomTitle) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
-
-  async hide(): Promise<void> {
-    super.hide();
-    await this.plugin.saveSettings();
-    this.plugin.handleFileOpen();
-  }
-
-  display(): void {
-    const { containerEl } = this;
-
-    const locales = window.moment.locales().reduce(
-      (acc, locale) => {
-        acc[locale] = locale;
-        return acc;
-      },
-      {} as Record<string, string>,
-    );
-
-    containerEl.empty();
-
-    new Setting(containerEl).setName("Language").addDropdown((cp) =>
-      cp
-        .addOptions(locales)
-        .setValue(this.plugin.settings.language)
-        .onChange((value) => {
-          this.plugin.settings.language = value;
-          window.moment.updateLocale(value, {
-            week: { dow: this.plugin.settings.firstDayOfWeek },
-          });
-        }),
-    );
-
-    new Setting(containerEl).setName("First day of week").addDropdown((cp) =>
-      cp
-        .addOption("7", "Sunday")
-        .addOption("1", "Monday")
-        .setValue(this.plugin.settings.firstDayOfWeek.toString())
-        .onChange((_value) => {
-          const value = parseInt(_value) as 1 | 7;
-          this.plugin.settings.firstDayOfWeek = value;
-          window.moment.updateLocale(this.plugin.settings.language, {
-            week: { dow: value },
-          });
-        }),
-    );
-
-    // [INFO] Periodic Notes Settings
-    (
-      [
-        ["Daily Notes", "daily"],
-        ["Weekly Notes", "weekly"],
-        ["Monthly Notes", "monthly"],
-        ["Yearly Notes", "yearly"],
-      ] as const
-    ).forEach(([name, key]) => {
-      const exampleDom = document.createElement("div");
-      const exampleFragment = document.createDocumentFragment();
-      exampleFragment.appendChild(exampleDom);
-
-      const render = () => renderExample(key, this.plugin.settings, exampleDom);
-
-      render();
-
-      // [INFO] Toggle
-      const titleSetting = new Setting(containerEl)
-        .setName(name)
-        .setHeading()
-        .addToggle((cp) =>
-          cp.setValue(this.plugin.settings[key].enabled).onChange((value) => {
-            this.plugin.settings[key].enabled = value;
-            if (!value) {
-              this.plugin.settings[key].setting = DEFAULT_PERIODIC_SETTING;
-            }
-            render();
-          }),
-        );
-      titleSetting.nameEl.style.fontSize = "1.5em";
-      titleSetting.nameEl.style.fontWeight = "bold";
-      titleSetting.nameEl.style.padding = "0.5em 0";
-      const exampleSetting = new Setting(containerEl)
-        .setName("Example")
-        .setDesc(exampleFragment);
-      const exampleSettingDescEl = exampleSetting.descEl;
-      exampleSetting.infoEl.childNodes[1].remove();
-      exampleSetting.controlEl.appendChild(exampleSettingDescEl);
-      exampleSetting.controlEl.style.textAlign = "start";
-
-      // [INFO] Date Format
-      const syntaxNode = document.createElement("a");
-      syntaxNode.href = "https://momentjs.com/docs/#/displaying/format/";
-      syntaxNode.textContent = "Syntax Reference";
-      const syntaxFragment = new DocumentFragment();
-      syntaxFragment.appendChild(syntaxNode);
-      new Setting(containerEl)
-        .setName("Date Format")
-        .setDesc(syntaxFragment)
-        .addMomentFormat((cp) =>
-          cp
-            .setValue(this.plugin.settings[key].setting.dateFormat)
-            .onChange((value) => {
-              this.plugin.settings[key].setting.dateFormat = value;
-              render();
-            }),
-        );
-
-      // [INFO] Date Format Not In This Year
-      new Setting(containerEl)
-        .setName("Date Format Not In This Year")
-        .setDesc(syntaxFragment)
-        .addMomentFormat((cp) =>
-          cp
-            .setValue(this.plugin.settings[key].setting.dateFormatNotInThisYear)
-            .onChange((value) => {
-              this.plugin.settings[key].setting.dateFormatNotInThisYear = value;
-              render();
-            }),
-        );
-
-      // [INFO] Separator
-      new Setting(containerEl).setName("Separator").addText((cp) =>
-        cp
-          .setValue(this.plugin.settings[key].setting.separator)
-          .onChange((value) => {
-            this.plugin.settings[key].setting.separator = value;
-            render();
-          }),
-      );
-
-      // [INFO] Relative Position
-      new Setting(containerEl).setName("Relative Position").addDropdown((cp) =>
-        cp
-          .addOption("before", "Before Date")
-          .addOption("after", "After Date")
-          .setValue(this.plugin.settings[key].setting.relativePosition)
-          .onChange((value) => {
-            this.plugin.settings[key].setting.relativePosition = value as
-              | "before"
-              | "after";
-            render();
-          }),
-      );
-
-      // [INFO] Relative Settings
-      new Setting(containerEl)
-        .setName("Relative Settings")
-        .setDesc("Format: diff | text | color(optional)")
-        .addTextArea((cp) =>
-          cp
-            .setPlaceholder(`0 | xxx\n-1 | xxx | red`)
-            .setValue(
-              this.plugin.settings[key].setting.relativeSettings
-                .map((setting) =>
-                  [setting.diff, setting.text, setting.color]
-                    .filter((s) => s !== undefined)
-                    .join(" | "),
-                )
-                .join("\n"),
-            )
-            .onChange((value) => {
-              const items = value.split("\n");
-              const relativeSettings = [];
-              for (const item of items) {
-                const [diff, text, color] = item
-                  .split("|")
-                  .map((i) => i.trim());
-                if (diff && text) {
-                  relativeSettings.push({
-                    diff: parseInt(diff),
-                    text,
-                    color,
-                  });
-                }
-              }
-              this.plugin.settings[key].setting.relativeSettings =
-                relativeSettings;
-              render();
-            }),
-        );
-    });
-  }
-}
-
-function renderExample(
-  type: PeriodicType,
-  settings: PeriodicCustomTitleSettings,
-  target: HTMLDivElement,
+async function getAllRelativeSetting(
+  app: App,
+  setting: PeriodicCustomTitleSettings,
 ) {
-  target.innerHTML = "";
-  if (!settings[type].enabled) {
-    return;
-  }
-  const typeUnit: Map<PeriodicType, moment.unitOfTime.DurationConstructor> =
-    new Map([
-      ["daily", "d"],
-      ["weekly", "w"],
-      ["monthly", "M"],
-      ["yearly", "y"],
-    ]);
-  const regularText = document.createElement("div");
-  regularText.textContent = "Regular Text";
-  regularText.style.fontWeight = "600";
-  regularText.style.padding = "4px 0";
-  target.appendChild(regularText);
-
-  const lastYear = document.createElement("div");
-  lastYear.style.paddingLeft = "12px";
-  const lastYearDate = window.moment().add(-1, "y").month(0).date(1);
-  renderTitle(
-    { type, date: lastYearDate },
-    settings[type].setting,
-    lastYear,
-    settings.language,
-  );
-  target.appendChild(lastYear);
-
-  const thisYear = document.createElement("div");
-  thisYear.style.paddingLeft = "12px";
-  const thisYearDate = window.moment().month(0).date(1);
-  renderTitle(
-    { type, date: thisYearDate },
-    settings[type].setting,
-    thisYear,
-    settings.language,
-  );
-  target.appendChild(thisYear);
-
-  const datas: PeriodicReturn[] = settings[type].setting.relativeSettings.map(
-    (setting) => {
-      const date = window.moment().add(setting.diff, typeUnit.get(type));
-      return { type, date };
+  let files: { text: string; file: TFile }[] = [];
+  let now = new Date();
+  const promises: Promise<void>[] = [];
+  (["daily", "weekly", "monthly", "yearly"] as PeriodicType[]).forEach(
+    (type) => {
+      if (setting[type].enabled) {
+        setting[type].setting.relativeSettings.forEach((s) => {
+          let date = new Date(
+            now.getFullYear() + (type === "yearly" ? s.diff : 0),
+            now.getMonth() + (type === "monthly" ? s.diff : 0),
+            now.getDate() + (type === "weekly" ? s.diff * 7 : s.diff),
+          );
+          promises.push(
+            (async () => {
+              let path = await getPathByDate(app, type, date);
+              let file = app.vault.getFileByPath(path);
+              if (file !== null) {
+                files.push({
+                  text: file.parent.path + "/" + file.basename + "/" + s.text,
+                  file,
+                });
+              }
+            })(),
+          );
+        });
+      }
     },
   );
-
-  if (datas.length > 0) {
-    const relativeText = document.createElement("div");
-    relativeText.textContent = "Relative Text";
-    relativeText.style.fontWeight = "600";
-    relativeText.style.padding = "4px 0";
-    target.appendChild(relativeText);
-  }
-
-  for (const data of datas) {
-    const dom = document.createElement("div");
-    dom.style.paddingLeft = "12px";
-    renderTitle(data, settings[type].setting, dom, settings.language);
-    target.appendChild(dom);
-  }
+  await Promise.all(promises);
+  return files;
 }
